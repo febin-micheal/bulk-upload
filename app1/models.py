be9a12404c9eb25626c8cbae903ef6f4361af847
Febin Micheal Antony <43706733+febin-micheal@users.noreply.github.com>
2022-08-25 17:28:19 +0530

    Add files via upload

--- app1/models.py
+++ app1/models.py
@@ -13,7 +13,7 @@ class Student(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
     gender = models.CharField(max_length=50)
-    grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
+    grade = models.ForeignKey(Grade, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
--- app1/views.py
+++ app1/views.py
@@ -6,34 +6,44 @@ from django.contrib import messages
 
 # Create your views here.
 
-def home(request):
+def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            excel_file = request.FILES['file']
+            f = request.FILES['file']
 
-            if not excel_file.name.endswith('.xlsx'):
+            if not f.name.endswith('.xlsx'):
                 messages.error(request, 'Please upload an excel file')
-                return redirect('home')
+                return redirect('upload-file')
 
-            wb = openpyxl.load_workbook(excel_file)
-            worksheet = wb.active
+            with open('app1/uploads/excel.xlsx', 'wb+') as destination:
+                    for chunk in f.chunks():
+                        destination.write(chunk)
 
-            objs = list()
-            for row in worksheet.iter_rows(min_row=2):
-                row_data = list()
-                for cell in row:
-                    row_data.append(str(cell.value))
-                if not Student.objects.filter(id=row_data[0]).exists():
-                    grade = Grade.objects.filter(code=row_data[5]).values('id')
-                    obj = Student(id=row_data[0], first_name=row_data[1], last_name=row_data[2], email=row_data[3], gender=row_data[4], grade_id=grade)
-                    objs.append(obj)
-
-            Student.objects.bulk_create(objs)
-
-            object_list = list(Student.objects.all().values('id', 'first_name', 'last_name', 'email', 'gender', 'grade__code'))
-            return render(request, 'index.html', {'object_list': object_list})
-
+            return redirect('bulk-upload')
     else:
         form = UploadFileForm()
-    return render(request, 'home.html', {'form': form})
+        return render(request, 'upload.html', {'form': form})
+
+def bulk_upload(request):
+    excel_file = 'app1/uploads/excel.xlsx'
+
+    wb = openpyxl.load_workbook(excel_file)
+    worksheet = wb.active
+
+    objs = []
+    for row in worksheet.iter_rows(min_row=2):
+        row_data = [str(cell.value) for cell in row]
+        if not Grade.objects.filter(code=row_data[5]).exists():
+            Grade.objects.create(code=row_data[5])
+        grade = Grade.objects.filter(code=row_data[5]).values('id')
+        if not Student.objects.filter(id=row_data[0]).exists():
+            obj = Student(id=row_data[0], first_name=row_data[1], last_name=row_data[2], email=row_data[3], gender=row_data[4], grade_id=grade)
+            objs.append(obj)
+
+    Student.objects.bulk_create(objs)
+    return redirect('list-view')
+
+def list_view(request):
+    object_list = list(Student.objects.all().values('id', 'first_name', 'last_name', 'email', 'gender', 'grade__code'))
+    return render(request, 'list-view.html', {'object_list': object_list})
