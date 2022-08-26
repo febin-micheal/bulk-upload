f07e9a6afe831ec41641e1219af393623675cb1e
Febin Micheal Antony <43706733+febin-micheal@users.noreply.github.com>
2022-08-26 23:00:01 +0530

    Add files via upload

--- app1/views.py
+++ app1/views.py
@@ -34,17 +34,44 @@ def bulk_upload(request):
     wb = openpyxl.load_workbook(excel_file)
     worksheet = wb.active
 
-    objs = []
+    # reduce db hits in expense of loops
+    
+    grade_list =  list(Grade.objects.values()) # hit 1
+    grade_codes = [grade['code'] for grade in grade_list]
+
+    grades = []
     for row in worksheet.iter_rows(min_row=2):
         row_data = [str(cell.value) for cell in row]
-        if not Grade.objects.filter(code=row_data[5]).exists():
-            Grade.objects.create(code=row_data[5])
-        grade = Grade.objects.filter(code=row_data[5]).values('id')
-        if not Student.objects.filter(id=row_data[0]).exists():
-            obj = Student(id=row_data[0], first_name=row_data[1], last_name=row_data[2], email=row_data[3], gender=row_data[4], grade_id=grade)
-            objs.append(obj)
 
-    Student.objects.bulk_create(objs)
+        if row_data[5] not in grade_codes:
+            grade = Grade(code=row_data[5])
+            grades.append(grade)
+
+    Grade.objects.bulk_create(grades) # hit 2
+    grade_list =  list(Grade.objects.values()) # hit 3
+
+    student_list =  list(Student.objects.values())  # hit 4    
+    student_ids = [student['id'] for student in student_list]
+
+    creates = []
+    updates = []
+    for row in worksheet.iter_rows(min_row=2):
+        row_data = [str(cell.value) for cell in row]
+
+        grade_id = [grade['id'] for grade in grade_list if grade['code'] == row_data[5]]        
+
+        id = int(row_data[0])
+        student = Student(id=id, first_name=row_data[1], last_name=row_data[2], email=row_data[3], gender=row_data[4], grade_id=grade_id[0])
+        if id in student_ids:
+            updates.append(student)
+        else:
+            creates.append(student)
+
+    if updates:
+        Student.objects.bulk_update(updates, fields=['first_name', 'last_name', 'email', 'gender', 'grade_id']) # hit 5
+
+    if creates:
+        Student.objects.bulk_create(creates)   # hit 6
     return redirect('list-view')
 
 def list_view(request):
